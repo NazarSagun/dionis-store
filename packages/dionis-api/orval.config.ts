af0a594fcae8c5cfa4c10f44e90b8fec0b6e5be9
NazarSagun <nazar.sagun@gmail.com>
2024-06-09 20:05:55 +0100

    Add open api spec for users and auth

--- packages/dionis-api/orval.config.ts
+++ packages/dionis-api/orval.config.ts
@@ -1,14 +1,27 @@
-module.exports = {
+import { defineConfig } from 'orval'
+
+export default defineConfig({
   dionis: {
     output: {
       mode: 'tags-split',
-      target: 'src/dionis.ts',
+      target: 'src/dionis',
       schemas: 'src/model',
       client: 'react-query',
       mock: true,
+      clean: true,
+      override: {
+        mutator: {
+          path: './instance.ts',
+          name: 'customInstance',
+        },
+        query: {
+          useInfinite: true,
+          useInfiniteQueryParam: 'pageNumber',
+        },
+      },
     },
     input: {
       target: './dionis.yaml',
     },
   },
-}
+})
