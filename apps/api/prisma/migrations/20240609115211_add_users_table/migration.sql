8f72c7af5910676f0a54e2b297c4a65967b6ca20
NazarSagun <nazar.sagun@gmail.com>
2024-06-09 13:01:56 +0100

    Add postgresql database migration

---
+++ apps/api/prisma/migrations/20240609115211_add_users_table/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "password" TEXT NOT NULL,
+    "refreshToken" TEXT,
+    "role" INTEGER NOT NULL DEFAULT 101,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_refreshToken_key" ON "User"("refreshToken");
