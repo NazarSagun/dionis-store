65c2b3f0ff2e56e0c5f4180723f4afb60a78a698
NazarSagun <nazar.sagun@gmail.com>
2024-08-28 19:42:26 +0100

    Add image and description to page id

--- apps/web/app/game/[id]/page.tsx
+++ apps/web/app/game/[id]/page.tsx
@@ -2,26 +2,62 @@
 
 import { useGetGame } from '@repo/dionis-api/src/dionis/default/default'
 import classes from '../../page.module.css'
+import styles from './page.module.css'
 import clsx from 'clsx'
 import { useThemeState } from '@/providers/theme'
 import { Loader } from '@/ui'
+import Image from 'next/image'
+import { useParams } from 'next/navigation'
 
 export default function GamePage() {
   const { state } = useThemeState()
+  const { id } = useParams()
 
-  const { data, isLoading, error, isError } = useGetGame(1)
+  const { data, isLoading, error, isError } = useGetGame(Number(id))
 
-  const containerStyles = clsx(classes.container, state.mode === 'light' ? classes.light : null)
+  const loaderStyles = clsx(classes.container, state.mode === 'light' ? classes.light : null)
+  const containerStyles = clsx(styles.container, state.mode === 'light' ? classes.light : null)
+  const infoStyles = clsx(styles.info, state.mode === 'light' ? classes.light : null)
 
   if (isLoading) {
     return (
-      <div className={containerStyles}>
+      <div className={loaderStyles}>
         <Loader />
       </div>
     )
   }
 
   if (data) {
-    return <div className={containerStyles}>{data.title}</div>
+    const informationData = [
+      { label: 'Title', value: data.title },
+      { label: 'Genre', value: data.genre },
+      { label: 'Platform', value: data.platform },
+      { label: 'Publisher', value: data.publisher },
+    ]
+
+    return (
+      <div className={containerStyles}>
+        <div>
+          <Image
+            alt={data.title as string}
+            width={500}
+            height={300}
+            src={data.thumbnail as string}
+          />
+        </div>
+        <div className={infoStyles}>
+          <h1>Description</h1>
+
+          <p>{data.short_description}</p>
+
+          {informationData.map((item) => (
+            <div key={item.label}>
+              <span>{item.label}: </span>
+              <h2>{item.value}</h2>
+            </div>
+          ))}
+        </div>
+      </div>
+    )
   }
 }
